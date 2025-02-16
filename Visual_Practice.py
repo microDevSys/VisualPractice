12e07edd36a45feca4687a376b95d91261ecbf48
microDevSys <76966490+microDevSys@users.noreply.github.com>
2025-02-16 19:18:39 +0100

    Update Visual_Practice.py
    add github link

--- Visual_Practice.py
+++ Visual_Practice.py
@@ -4,6 +4,7 @@ from PySide6.QtGui import *
 from PySide6.QtCore import *
 
 #Visual Practice rev0.1 © Guillaume Sahuc fev/2025 
+#https://github.com/microDevSys/VisualPractice
 #https://creativecommons.org/licenses/by-nc-nd/4.0/deed.en
 
 class ZoomableGraphicsView(QGraphicsView):
